881a7798e04238bfd425dc0e38a7a1bbe31987ec
alexismengual <lexmengual@gmail.com>
2020-03-11 20:06:51 +0100

    First Commit

---
+++ WebApiExample/Controllers/StudentsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EntityFrameworkDatabase;
+
+namespace WebApiExample.Controllers
+{
+    public class StudentsController : ApiController
+    {
+
+        private VuelingEntities vuelingEntities = new VuelingEntities();
+
+        [HttpPost]
+        public IHttpActionResult Add()
+        {
+            Students student = new Students();
+            student.Name = "Postman";
+            vuelingEntities.Students.Add(student);
+            return Ok(Get());
+        }
+
+        [HttpGet]
+        public IEnumerable<Students> Get()
+        {
+            return vuelingEntities.Students.ToList();
+        }
+    }
+}
